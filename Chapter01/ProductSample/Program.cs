a46ba8f0ffdd82fb3ac0742bd5a2ad8432957105
KakinumaDaiki <oibc35145@ojs.ac.jp>
2024-04-18 13:37:20 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter01/ProductSample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Product Karinto = new Product(123, "かりんとう", 180);
+            Product daifuku = new Product(166, "大福", 204);
+
+            int price = Karinto.Price;//税抜き
+            int taxIncluded = Karinto.GetPriceIncludingTax();//税込み
+
+            int daifukuPrice = daifuku.Price;
+            int daifukuTaxIncluded = daifuku.GetPriceIncludingTax();
+
+            Console.WriteLine( Karinto.Name + "の税込み金額" 
+                                        + taxIncluded + "円[税抜き" + price + " 円]");
+
+            Console.WriteLine(daifuku.Name + "の税込み金額"
+                                        + daifukuTaxIncluded + "円[税抜き" + daifukuPrice + " 円]");
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Chapter01/Sample_p38/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_p38 {
+    public class Program {
+        static void Main(string[] args) {
+            Employee employee = new Employee {
+                Id = 100,
+                Name = "山田太郎",
+                Birthday = new DateTime(1992, 4, 5),
+                DivisionName = "第一営業部",
+            };
+            Student student = new Student {
+                Name = "柿沼大貴",
+                Birthday = new DateTime(2004, 5,31),
+                Grade = 2,
+                Classnumber = 1
+               
+            };
+            Console.WriteLine("{0}({1})は、{2}に所属しています。",
+                employee.Name,employee.GetAge(),employee.DivisionName);
+
+
+            Console.WriteLine("{0}-{2}年{3}組 {1:yyyy/M/d}生まれ。",
+                student.Name,student.Birthday,student.Grade,student.Classnumber);
+
+            Person person = student;
+            if (person is Student)
+                Console.WriteLine("person is Student");
+
+            object obj = student;
+            if (obj is Student)
+                Console.WriteLine("obj is Student");
+        }
+    }
+}
