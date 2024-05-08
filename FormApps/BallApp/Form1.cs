c3f5874c9a4fbd9d0f077b4ccaa4c662eae7b267
KakinumaDaiki <oibc35145@ojs.ac.jp>
2024-05-08 14:19:27 +0900

    途中

--- FormApps/BallApp/Form1.cs
+++ FormApps/BallApp/Form1.cs
@@ -1,5 +1,6 @@
 namespace BallApp {
     public partial class Form1 : Form {
+        TennisBall tennisBall;
         SoccerBall soccerBall;
         PictureBox pb;
 
@@ -18,6 +19,8 @@ namespace BallApp {
         private void timer1_Tick(object sender, EventArgs e) {
             soccerBall.Move();
             pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
+            tennisBall.Move();
+            pb.Location = new Point((int)tennisBall.PosX, (int)tennisBall.PosY);
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs e) {
@@ -25,14 +28,27 @@ namespace BallApp {
             pb = new PictureBox();//画像を表示するコンソール
             pb.Size = new Size(50, 50);
 
-            soccerBall = new SoccerBall(e.X,e.Y);
+            if(e.Button == MouseButtons.Left) {
+                soccerBall = new SoccerBall(e.X - 25, e.Y);
+                pb.Image = soccerBall.Image;
+                pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                pb.Parent = this;
+                timer1.Start();
+            }
+            
+            if(e.Button == MouseButtons.Right) {
+                tennisBall = new TennisBall(e.X, e.Y);
+                pb.Image = tennisBall.Image;
+                pb.Location = new Point((int)tennisBall.PosX, (int)tennisBall.PosY);
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                pb.Parent = this; 
+                timer1.Start();
+            }
+            
 
-            pb.Image = soccerBall.Image;
-            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
-            pb.SizeMode = PictureBoxSizeMode.StretchImage;
-            pb.Parent = this;
 
-            timer1.Start();
+           
         }
     }
 }
