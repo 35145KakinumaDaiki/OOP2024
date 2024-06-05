b969c592bab20e3d6186340f1789173e6d49a859
KakinumaDaiki <oibc35145@ojs.ac.jp>
2024-06-05 13:11:46 +0900

    第6章演習問題

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var books = Books.GetBooks().OrderByDescending(x => x.Price ).ToList();
+
+            books.ForEach(b => Console.WriteLine(b.Title + ":" + b.Price));
+            //foreach ( var book in books ) {
+              //  Console.WriteLine( book.Title + ":" + book.Price );
+            //}
+                                                                                                     
+        }
+    }
+}
