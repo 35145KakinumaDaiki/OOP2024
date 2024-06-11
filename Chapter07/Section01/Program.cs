ab1997c4fe705b8276e0c2fccafe178a6252a913
KakinumaDaiki <oibc35145@ojs.ac.jp>
2024-06-11 14:21:54 +0900

    上書き処理

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,25 +14,62 @@ internal class Program {
 
         Console.WriteLine("県庁所在地の登録");
         for (int i = 0; i < 5; i++) {
+
+            //都道府県の入力
             Console.Write("都道府県:");
             string prefecture = Console.ReadLine();
 
+            //県庁所在地の入力
             Console.Write("県庁所在地");
             string city = Console.ReadLine();
 
-            if (prefecture.Contains(Console.ReadLine()) || city.Contains(Console.ReadLine())) {
-                Console.WriteLine("すでに登録済みです");
+            //すでに都道府県が登録されているか
+            if (prefectureOfficeDict.ContainsKey(prefecture) || prefectureOfficeDict.ContainsKey(city)) {
+                Console.WriteLine("上書きしますか？");
+                string ans = Console.ReadLine();
+                if (ans == "yes") {
+                    prefectureOfficeDict[prefecture] = city;
+                    i -= 1;
+                }
             } else {
                 prefectureOfficeDict[prefecture] = city;
             }
-
-
-
         }
 
-        foreach (var item in prefectureOfficeDict) {
-            Console.WriteLine($"{item.Key}の県庁所在地は{item.Value}です");
+        Boolean endFlag = false;//終了フラグ
+        while (true) {
+            Console.WriteLine("****メニュー****");
+            Console.WriteLine("1:一覧表示");
+            Console.WriteLine("2:検索");
+            Console.WriteLine("9:終了");
+            Console.Write(">");
+            string menuSelect = Console.ReadLine();
+            switch(menuSelect) {
+                case "1":
+                    //一覧処理
+                    foreach (var item in prefectureOfficeDict) {
+                        Console.WriteLine($"{item.Key}の県庁所在地は{item.Value}です");
+                    }
+                    break;
+
+                case "2":
+                    //都道府県の入力
+                    Console.Write("都道府県:");
+                    string prefecture = Console.ReadLine();
+                    Console.WriteLine(prefecture + "の県庁所在地は" + prefectureOfficeDict[prefecture] + "です。");
+                    break;
+
+                case "9":
+                    endFlag = true;//終了フラグON
+                    break;
+
+            }
+            if (endFlag) {
+                break;
+            }
         }
+
+       
 
 
 
