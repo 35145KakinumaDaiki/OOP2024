696e2badd48dabfb1c46034e3743fc659e6295e0
KakinumaDaiki <oibc35145@ojs.ac.jp>
2024-06-18 11:17:13 +0900

    「設問１_提出」

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Xml.Linq;
 
 namespace Test01 {
     class ScoreCounter {
@@ -13,7 +15,18 @@ namespace Test01 {
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-
+            List<Student> students = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (var line in lines) {
+                string[] items = line.Split(',');
+                Student student = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                students.Add(student);
+            }
+            return students;
 
 
 
@@ -22,11 +35,14 @@ namespace Test01 {
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
-
-
-
-
-
+            var dict = new Dictionary<string, int>();
+            foreach (var student in _score) {
+                if (dict.ContainsKey(student.Name))
+                    dict[student.Name] += student.Score;
+                else
+                    dict[student.Name] = student.Score;
+            }
+            return dict;
         }
     }
 }
