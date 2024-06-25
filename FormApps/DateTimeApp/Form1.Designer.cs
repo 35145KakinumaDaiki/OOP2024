2077f027705a99e29799e23d3ca52b7467c52ab4
KakinumaDaiki <oibc35145@ojs.ac.jp>
2024-06-25 11:08:16 +0900

    終了

--- FormApps/DateTimeApp/Form1.Designer.cs
+++ FormApps/DateTimeApp/Form1.Designer.cs
@@ -24,13 +24,13 @@
         /// </summary>
         private void InitializeComponent() {
             label1 = new Label();
-            dtpBirthday = new DateTimePicker();
+            dtpDate = new DateTimePicker();
             btDateCount = new Button();
             tbDisp = new TextBox();
             nudDay = new NumericUpDown();
             btDayBefore = new Button();
             btDayafter = new Button();
-            yearsold = new Button();
+            btAge = new Button();
             ((System.ComponentModel.ISupportInitialize)nudDay).BeginInit();
             SuspendLayout();
             // 
@@ -45,12 +45,12 @@
             label1.TabIndex = 0;
             label1.Text = "日付";
             // 
-            // dtpBirthday
+            // dtpDate
             // 
-            dtpBirthday.Location = new Point(268, 67);
-            dtpBirthday.Name = "dtpBirthday";
-            dtpBirthday.Size = new Size(200, 29);
-            dtpBirthday.TabIndex = 1;
+            dtpDate.Location = new Point(268, 67);
+            dtpDate.Name = "dtpDate";
+            dtpDate.Size = new Size(200, 29);
+            dtpDate.TabIndex = 1;
             // 
             // btDateCount
             // 
@@ -101,27 +101,29 @@
             btDayafter.UseVisualStyleBackColor = true;
             btDayafter.Click += btDayafter_Click;
             // 
-            // yearsold
+            // btAge
             // 
-            yearsold.Location = new Point(480, 216);
-            yearsold.Name = "yearsold";
-            yearsold.Size = new Size(116, 50);
-            yearsold.TabIndex = 6;
-            yearsold.Text = "年齢";
-            yearsold.UseVisualStyleBackColor = true;
+            btAge.Font = new Font("Yu Gothic UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            btAge.Location = new Point(480, 216);
+            btAge.Name = "btAge";
+            btAge.Size = new Size(116, 50);
+            btAge.TabIndex = 6;
+            btAge.Text = "年齢";
+            btAge.UseVisualStyleBackColor = true;
+            btAge.Click += btAge_Click;
             // 
             // old
             // 
             AutoScaleDimensions = new SizeF(9F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(659, 492);
-            Controls.Add(yearsold);
+            Controls.Add(btAge);
             Controls.Add(btDayafter);
             Controls.Add(btDayBefore);
             Controls.Add(nudDay);
             Controls.Add(tbDisp);
             Controls.Add(btDateCount);
-            Controls.Add(dtpBirthday);
+            Controls.Add(dtpDate);
             Controls.Add(label1);
             Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
             Margin = new Padding(4);
@@ -135,12 +137,12 @@
         #endregion
 
         private Label label1;
-        private DateTimePicker dtpBirthday;
+        private DateTimePicker dtpDate;
         private Button btDateCount;
         private TextBox tbDisp;
         private NumericUpDown nudDay;
         private Button btDayBefore;
         private Button btDayafter;
-        private Button yearsold;
+        private Button btAge;
     }
 }
--- FormApps/DateTimeApp/Form1.cs
+++ FormApps/DateTimeApp/Form1.cs
@@ -7,22 +7,31 @@ namespace DateTimeApp {
         public void btDateCount_Click(object sender, EventArgs e) {
 
             var Today = DateTime.Today;
-            TimeSpan diff = Today - dtpBirthday.Value;
+            TimeSpan diff = Today - dtpDate.Value;
 
             tbDisp.Text = (diff.Days + 1) + "“ú–Ú";
         }
 
 
         private void btDayafter_Click(object sender, EventArgs e) {
-            var Today = DateTime.Today;
-            var future = Today.AddDays((double)nudDay.Value);
+            var future = dtpDate.Value.AddDays((double)nudDay.Value);
             tbDisp.Text = future.ToString();
         }
 
         private void btDayBefore_Click(object sender, EventArgs e) {
-            var Today = DateTime.Today;
-            var past = Today.AddDays((double)-nudDay.Value);
+            var past = dtpDate.Value.AddDays((double)-nudDay.Value);
             tbDisp.Text = past.ToString();
+        }
+
+        private void btAge_Click(object sender, EventArgs e) {
+            var Today = DateTime.Today;
+            var birthday = dtpDate.Value;
+            var age = Today.Year - birthday.Year;
+            if (Today < birthday.AddYears(age)) {
+                age--;
+            }
+            tbDisp.Text = age.ToString();
+
         }
     }
 }
