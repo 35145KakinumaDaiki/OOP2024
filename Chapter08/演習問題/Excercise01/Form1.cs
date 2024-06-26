61e6ed9e5bd28355e3dddd54c33d36508738e713
KakinumaDaiki <oibc35145@ojs.ac.jp>
2024-06-26 14:49:08 +0900

    演習問題８終了

--- Chapter08/演習問題/Excercise01/Form1.cs
+++ Chapter08/演習問題/Excercise01/Form1.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using System.Globalization;
 namespace Excercise01 {
     public partial class Form1 : Form {
@@ -11,19 +12,54 @@ namespace Excercise01 {
             culture.DateTimeFormat.Calendar = new JapaneseCalendar();
             var datOfweek = culture.DateTimeFormat.GetDayName(date.DayOfWeek);
             tbDisp.Text = date.ToString("yyyy/M/d/ HH:mm" + "\r\n" + "yyyy”NMMŒŽdd“ú HHŽžmm•ªss•b" + "\r\n" + "ggyy”NMŒŽd“ú(dddd)", culture);
-            
 
-           
+
+
 
         }
 
         private void btEx8_2_Click(object sender, EventArgs e) {
-            static DateTime NextDay(DateTime date,DayOfWeek dayOfWeek) {
-                var days = (int)dayOfWeek - (int)(date.DayOfWeek);
-                if (days <= 0)
-                    days += 7;
-                return date.AddDays(days);
+
+
+            var dateTime = DateTime.Today;
+            foreach (var dayofweek in Enum.GetValues(typeof(DayOfWeek))) {
+
+                var str = string.Format("{0:yy/MM/dd}‚ÌŽŸT‚Ì{1}", dateTime, (DayOfWeek)dayofweek);
+                //—ˆT‚Ì“ú•t‚ðŽæ“¾
+                var str2 = string.Format("{0:yy/MM/dd(ddd)}", NextWeek(dateTime, (DayOfWeek)dayofweek));
+
+                tbDisp.Text += str + str2 + "\r\n";
             }
+        }
+
+        public static DateTime NextWeek(DateTime date, DayOfWeek dayofweek) {
+            var nextweek = date.AddDays(7);
+            var day = (int)dayofweek - (int)date.DayOfWeek;
+            return nextweek.AddDays(day);
+        }
+
+        private void btEx8_3_Click(object sender, EventArgs e) {
+            var tw = new TimeWatch();
+            tw.Start();
+            Thread.Sleep(1000);
+            TimeSpan duration = tw.Stop();
+            var str = String.Format("ˆ—ŽžŠÔ‚Í{0}ƒ~ƒŠ•b‚Å‚µ‚½", duration.TotalMilliseconds);
+            tbDisp.Text = str;
+        }
+    }
+
+    class TimeWatch {
+        private DateTime _time;
+
+        public void Start() {
+            _time = DateTime.Now;
+
+
+        }
+
+        public TimeSpan Stop() {
+            return DateTime.Now - _time;
+
         }
     }
 }
