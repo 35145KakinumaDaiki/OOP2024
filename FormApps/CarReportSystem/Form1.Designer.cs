901542409dd5dd92abaeab299f8222e18f74ce3f
KakinumaDaiki <oibc35145@ojs.ac.jp>
2024-06-26 15:59:29 +0900

    カーレポート　途中

--- FormApps/CarReportSystem/Form1.Designer.cs
+++ FormApps/CarReportSystem/Form1.Designer.cs
@@ -264,6 +264,7 @@
             btAddReport.TabIndex = 6;
             btAddReport.Text = "追加";
             btAddReport.UseVisualStyleBackColor = true;
+            btAddReport.Click += btAddReport_Click;
             // 
             // btModifyReport
             // 
