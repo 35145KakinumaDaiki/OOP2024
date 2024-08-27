e6c8e7d43c4b30685ff5a4b5f5939f04c5e3cf6b
KakinumaDaiki <oibc35145@ojs.ac.jp>
2024-08-27 12:06:56 +0900

    2-2

--- Chapter12/演習問題/Exersise01/Employee.cs
+++ Chapter12/演習問題/Exersise01/Employee.cs
@@ -1,16 +1,16 @@
 
 
 using System;
+namespace Exersise01 {
+    public class Employee {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime HireDate { get; set; }
+        public override string ToString() {
+            return string.Format("[ID={0}, Name={1}, HireDate={2}]", Id, Name, HireDate);
+        }
 
-public class Employee {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public DateTime HireDate { get; set; }
-    public override string ToString() {
-        return string.Format("[ID={0}, Name={1}, HireDate={2}]",Id,Name,HireDate);
     }
-
-   
 }
 
 
--- Chapter12/演習問題/Exersise01/Program.cs
+++ Chapter12/演習問題/Exersise01/Program.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
+using System.Runtime.Serialization;
+
 
 namespace Exersise01 {
     internal class Program {
@@ -35,18 +37,46 @@ namespace Exersise01 {
                 }
             };
 
-            using(var writer = XmlWriter.Create("employee.xml")) {
+            using(var writer = XmlWriter.Create(outfile)) {
                 var serializer = new XmlSerializer(emps.GetType());
                 serializer.Serialize(writer, emps);
             }
         }
 
         private static void Exercise1_2(string outfile) {
-            
+            var emps = new Employee[] {
+                new Employee {
+                    Id = 123,
+                    Name = "出井　秀幸",
+                    HireDate= new DateTime(2001,3,1),
+                },
+                new Employee {
+                    Id=1,
+                    Name = "長谷部　勇気",
+                    HireDate= new DateTime(1998 ,11,27),
+                }
+            };
+            var settings = new XmlWriterSettings {
+                Encoding = new System.Text.UTF8Encoding(false),
+                Indent = true,
+                IndentChars = "  ",
+            };
+            using(var writer = XmlWriter.Create(outfile, settings)) {
+                var serializer = new DataContractSerializer(emps.GetType());
+                serializer.WriteObject(writer, emps);
+            }
         }
 
         private static void Exercise1_3(string file) {
-            
+            using(var reader = XmlReader.Create(file)) {
+                var serializer = new DataContractSerializer(typeof(Employee[]));
+                var employee = serializer.ReadObject(reader) as Employee[];
+                foreach (var emp in emps) {
+                    Console.WriteLine("{0} {1} {2}",emp.Id,emp.Name,emp.HireDate);
+                }
+            }
+
+           
         }
 
         private static void Exercise1_4(string file) {
