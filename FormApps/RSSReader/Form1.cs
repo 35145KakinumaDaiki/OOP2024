ce172863db023e822cb544cbc272127d714cd877
KakinumaDaiki <oibc35145@ojs.ac.jp>
2024-09-03 11:24:43 +0900

    タイトルを表示

---
+++ FormApps/RSSReader/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RSSReader {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btGet_Click(object sender, EventArgs e) {
+            using (var wc = new WebClient()) {
+                var url = wc.OpenRead(tbRssUrl.Text);
+                var xdoc = XDocument.Load(url);
+                var xelements = xdoc.Descendants("item").Descendants("title");
+                foreach(var xnovelist in xelements) {
+                    var xtitle = xnovelist.Attribute("title");
+                    lbRssTitle.Items.Add(xnovelist.Value);
+                    
+                }
+
+            }
+        }
+    }
+}
